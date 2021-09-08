d10b239e4bbd8440fb2e81cfc35ebbf8e20ec24f
Leejin-Yang <ejin1996@gmail.com>
2021-09-08 14:47:09 +0900

    feat: Create grid cards

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Row } from "antd";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import axios from "axios";
 import MainImage from "./Sections/MainImage";
+import GridCards from "../commons/GridCards";
 
 function LandingPage() {
   const [Movies, setMovies] = useState([]);
@@ -11,7 +13,7 @@ function LandingPage() {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
     axios.get(endpoint).then((response) => {
-      setMovies([response.data.results]);
+      setMovies(response.data.results);
       setMainMovieImage(response.data.results[0]);
     });
   }, []);
@@ -32,6 +34,22 @@ function LandingPage() {
         <hr />
 
         {/* Movie Gird Cards */}
+        <Row gutter={[16, 16]}>
+          {Movies &&
+            Movies.map((movie, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  image={
+                    movie.poster_path
+                      ? `${IMAGE_BASE_URL}w500/${movie.poster_path}`
+                      : null
+                  }
+                  movieId={movie.id}
+                  movieName={movie.original_title}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
       </div>
 
       <div style={{ display: "flex", justifyContent: "center" }}>
