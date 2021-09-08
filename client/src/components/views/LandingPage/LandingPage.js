1ee1027a97c80f97c4f21bb564bab6e16327abab
Leejin-Yang <ejin1996@gmail.com>
2021-09-08 14:13:32 +0900

    feat: Create main image in landing page

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import axios from "axios";
+import MainImage from "./Sections/MainImage";
+
+function LandingPage() {
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    axios.get(endpoint).then((response) => {
+      setMovies([response.data.results]);
+      setMainMovieImage(response.data.results[0]);
+    });
+  }, []);
+
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* Main Image */}
+      {MainMovieImage && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280/${MainMovieImage.backdrop_path}`}
+          title={MainMovieImage.original_title}
+          text={MainMovieImage.overview}
+        />
+      )}
+
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movies by Latest</h2>
+        <hr />
+
+        {/* Movie Gird Cards */}
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>Load More</button>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
